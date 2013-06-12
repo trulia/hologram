a3c0756d8616627fe8a82503be14730bb6ee788c
JD Cantrell <jcantrell@trulia.com>
2013-06-12 16:14:22 -0700

    Bump up version

--- lib/hologram/version.rb
+++ lib/hologram/version.rb
@@ -11,7 +11,7 @@
 #    * Neither the name of the Trulia, Inc. nor the
 #      names of its contributors may be used to endorse or promote products
 #      derived from this software without specific prior written permission.
-#
+# 
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
 # IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
 # TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
@@ -25,5 +25,5 @@
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 module Hologram
-  VERSION = "0.5.1"
+  VERSION = "0.5.2"
 end
