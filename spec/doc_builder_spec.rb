6a36b799cc084832e4bed93db27c1aa1abbc470e
Johny <jho406@gmail.com>
2014-01-05 17:29:00 -0500

    Add doc builder test

---
+++ spec/doc_builder_spec.rb
@@ -0,0 +1,102 @@
+require 'spec_helper'
+
+describe Hologram::DocBuilder do
+  subject(:builder) { Hologram::DocBuilder.new }
+
+  # Rails Kernerl helper
+  def silence_stream(stream)
+    old_stream = stream.dup
+    stream.reopen(RbConfig::CONFIG['host_os'] =~ /mswin|mingw/ ? 'NUL:' : '/dev/null')
+    stream.sync = true
+    yield
+  ensure
+    stream.reopen(old_stream)
+  end
+
+  context '#init' do
+    around do |example|
+      silence_stream(STDOUT) do
+        example.run
+      end
+    end
+
+    context 'when passed an invalid config' do
+      before do
+        File.open('bad_config.yml', 'w'){ |io| io << '%' }
+      end
+
+      after do
+        FileUtils.rm('bad_config.yml')
+      end
+
+      it 'exits the process' do
+        expect { builder.init(['bad_config.yml']) }.to raise_error SystemExit
+      end
+    end
+
+    context 'when passed a config file' do
+      let(:style_files) { Dir[File.expand_path('../fixtures/styleguide/**/*.*', __FILE__)] }
+      let(:config_path) { File.join(Dir.pwd, 'spec/fixtures/source/config.yml') }
+      let(:config_copy_path) { File.join(Dir.pwd, 'spec/fixtures/source/config.yml.copy') }
+
+      around do |example|
+        Dir.mktmpdir do |tmpdir|
+          FileUtils.cp(config_path, config_copy_path)
+          File.open(config_copy_path, 'a'){ |io| io << "destination: #{tmpdir}" }
+          current_dir = Dir.pwd
+          Dir.chdir(tmpdir)
+
+          example.run
+
+          Dir.chdir(current_dir)
+          FileUtils.rm(config_copy_path)
+        end
+      end
+
+      before do
+        builder.init([config_copy_path])
+      end
+
+      it 'builds a styleguide' do
+        processed_files = Dir[File.join('.', '**/*.*')]
+        processed_files.each_with_index do |file, index|
+          expect(FileUtils.cmp(file, style_files[index])).to be_true
+        end
+      end
+    end
+
+    context 'when passed "init" as arg' do
+      around do |example|
+        Dir.mktmpdir do |dir|
+          Dir.chdir(dir) do
+            example.run
+          end
+        end
+      end
+
+      before do
+        builder.init(['init'])
+      end
+
+      it 'creates a config file' do
+        expect(File.exists?('hologram_config.yml')).to be_true
+      end
+
+      it 'creates default assets' do
+        Dir.chdir('doc_assets') do
+          ['_header.html', '_footer.html'].each do |asset|
+            expect(File.exists?(asset)).to be_true
+          end
+        end
+      end
+
+      context 'when a hologram_config.yml already exists' do
+        it 'does nothing' do
+          open('hologram_config.yml', 'w') {|io|io << 'foo'}
+          builder.init(['init'])
+          expect(IO.read('hologram_config.yml')).to eql('foo')
+        end
+      end
+    end
+  end
+end
---
+++ spec/fixtures/source/components/index.md
@@ -0,0 +1,23 @@
+#Welcome
+
+This style guide is an example of what
+[hologram](http://trulia.github.io/hologram)  can help you build and
+maintain.
+
+The goal of hologram is to make it easy to document your CSS and to
+display the code examples to use that CSS. Hologram has no
+opinions about how you should actually organize/style your style guide.
+You could do everything as a single file with no header/footer and it
+would work just fine. Or, you could break it up into an individual file
+for each component. Top navigation, left navigation, no
+navigation....that's up to you. Build it however you'd like.  
+
+
+We've borrowed some of [Trulia](http://trulia.com)'s own CSS library to
+demonstrate how hologram can be used. If you want more details about
+hologram see the [git repository](http://github.com/trulia/hologram).  
+
+
+This is a work in progress, please consider contributing to
+[hologram](http://github.com/trulia/hologram).
+
