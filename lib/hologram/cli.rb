6544a0524e0d3e8c0b0a09e885ef43899a6e4d7e
JD Cantrell <jdcantrell@gmail.com>
2014-05-01 11:02:45 -0700

    Handle CommentLoadError

--- lib/hologram/cli.rb
+++ lib/hologram/cli.rb
@@ -37,6 +37,8 @@ module Hologram
       builder = DocBuilder.from_yaml(config)
       DisplayMessage.error(builder.errors.first) if !builder.is_valid?
       builder.build
+    rescue CommenLoadError => e
+      DisplayMessage.error(e.message)
     rescue Errno::ENOENT
       DisplayMessage.error("Could not load config file, try 'hologram init' to get started")
     end
