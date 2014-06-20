2e93eedb4ec899f5ce61015a42798a4cfe7d5f62
JD Cantrell <jcantrell@trulia.com>
2014-06-20 14:46:24 -0700

    Remove pygments, add rouge, and bump redcarpet

--- lib/hologram/markdown_renderer.rb
+++ lib/hologram/markdown_renderer.rb
@@ -1,16 +1,20 @@
 module Hologram
   class MarkdownRenderer < Redcarpet::Render::HTML
     def block_code(code, language)
+      formatter = Rouge::Formatters::HTML.new(wrap: false)
       if language and language.include?('example')
         if language.include?('js')
+          lexer = Rouge::Lexer.find('js')
           # first actually insert the code in the docs so that it will run and make our example work.
           '<script>' + code + '</script>
-          <div class="codeBlock jsExample">' + Pygments.highlight(code) + '</div>'
+          <div class="codeBlock jsExample"><pre>' + formatter.format(lexer.lex(code)) + '</pre></div>'
         else
-          '<div class="codeExample">' + '<div class="exampleOutput">' + render_html(code, language) + '</div>' + '<div class="codeBlock">' + Pygments.highlight(code, :lexer => get_lexer(language)) + '</div>' + '</div>'
+          lexer = Rouge::Lexer.find(get_lexer(language))
+          '<div class="codeExample">' + '<div class="exampleOutput">' + render_html(code, language) + '</div>' + '<div class="codeBlock"><pre>' + formatter.format(lexer.lex(code)) + '</pre></div>' + '</div>'
         end
       else
-        '<div class="codeBlock">' + Pygments.highlight(code) + '</div>'
+        lexer = Rouge::Lexer.find_fancy('guess', code)
+        '<div class="codeBlock"><pre>' + formatter.format(lexer.lex(code)) + '</pre></div>'
       end
     end
 
