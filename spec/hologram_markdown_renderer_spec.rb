e20ad88214685eca2498141eb6387833b8ca9f75
Todd Sedano <pair+tsedano@pivotallabs.com>
2014-07-17 11:55:34 -0700

    Removing slim specific test case

--- spec/hologram_markdown_renderer_spec.rb
+++ spec/hologram_markdown_renderer_spec.rb
@@ -11,19 +11,4 @@ describe HologramMarkdownRenderer do
       expect(TestFoo.ancestors).to include Hologram::MarkdownRenderer
     end
   end
-
-  context 'slim' do
-    it 'can render slim' do
-      expect(subject.block_code('button button_text', 'slim_example')).to eq "<div class=\"codeExample\">" +
-                                                                                 "<div class=\"exampleOutput\">" +
-                                                                                    "<button>button_text</button>" +
-                                                                                 "</div>" +
-                                                                                 "<div class=\"codeBlock\">" +
-                                                                                  "<div class=\"highlight\">" +
-                                                                                    "<pre>button button_text\n</pre>" +
-                                                                                  "</div>" +
-                                                                                 "</div>" +
-                                                                              "</div>"
-    end
-  end
 end
