a947a77e894ba84043d2178f0257ff36c22811a0
Geoff Pleiss <gpleiss@pivotal.io>
2014-12-22 11:19:03 -0800

    Refactor template rendering code
    - Create classes for each type of code example
    - Use ERB for template rendering
    
    Signed-off-by: Bebe Peng <bpeng@pivotal.io>

--- lib/hologram/block_code_renderer.rb
+++ lib/hologram/block_code_renderer.rb
@@ -1,44 +1,128 @@
+require 'erb'
+
 module Hologram
   class BlockCodeRenderer < Struct.new(:code, :markdown_language)
     def render
       if is_html? || is_haml? || is_slim?
         if is_table?
-          [
-            "<div class=\"codeTable\">",
-              "<table>",
-                "<tbody>",
-                  code_example_rows,
-                "</tbody>",
-              "</table>",
-            "</div>",
-          ].join('')
+          if is_html?
+            examples = code.split("\n\n").map { |code_snippit| HtmlExample.new(code_snippit) }
+          elsif is_haml?
+            examples = code.split("\n\n").map { |code_snippit| HamlExample.new(code_snippit) }
+          elsif is_slim?
+            examples = code.split("\n\n").map { |code_snippit| SlimExample.new(code_snippit) }
+          end
+          ERB.new(code_table_template).result(binding)
         else
-          [
-            "<div class=\"codeExample\">",
-              example_output(code),
-              code_block(code),
-            "</div>"
-          ].join('')
+          if is_html?
+            example = HtmlExample.new(code)
+          elsif is_haml?
+            example = HamlExample.new(code)
+          elsif is_slim?
+            example = SlimExample.new(code)
+          end
+          ERB.new(code_example_template).result(example.get_binding)
         end
 
       elsif is_js?
-        [
-          "<script>#{code}</script> ",
-          code_block(code, extra_classes: ['jsExample'])
-        ].join('')
+        example = JsExample.new(code)
+        ERB.new(js_example_template).result(example.get_binding)
 
       elsif is_jsx?
-        [
-          "<script type='text/jsx'>#{code}</script> ",
-          code_block(code, extra_classes: ['jsExample'])
-        ].join('')
+        example = JsxExample.new(code)
+        ERB.new(jsx_example_template).result(example.get_binding)
 
       else
-        code_block(code)
+        example = Example.new(code)
+        ERB.new(unknown_example_template).result(example.get_binding)
       end
     end
 
     private
+
+    def code_example_template
+      [
+        "<div class=\"codeExample\">",
+          "<div class=\"exampleOutput\">",
+            "<%= rendered_example %>",
+          "</div>",
+          "<div class=\"codeBlock\">",
+            "<div class=\"highlight\">",
+              "<pre>",
+                "<%= code_example %>",
+              "</pre>",
+            "</div>",
+          "</div>",
+        "</div>"
+      ].join('')
+    end
+
+    def code_table_template
+      [
+        "<div class=\"codeTable\">",
+          "<table>",
+            "<tbody>",
+              "<% examples.each do |example| %>",
+                "<tr>",
+                  "<th>",
+                    "<div class=\"exampleOutput\">",
+                      "<%= example.rendered_example %>",
+                    "</div>",
+                  "</th>",
+                  "<td>",
+                    "<div class=\"codeBlock\">",
+                      "<div class=\"highlight\">",
+                        "<pre>",
+                          "<%= example.code_example %>",
+                        "</pre>",
+                      "</div>",
+                    "</div>",
+                  "</td>",
+                "</tr>",
+              "<% end %>",
+            "</tbody>",
+          "</table>",
+        "</div>",
+      ].join('')
+    end
+
+    def js_example_template
+      [
+        "<script><%= rendered_example %></script> ",
+        "<div class=\"codeBlock jsExample\">",
+          "<div class=\"highlight\">",
+            "<pre>",
+              "<%= code_example %>",
+            "</pre>",
+          "</div>",
+        "</div>",
+      ].join('')
+    end
+
+    def jsx_example_template
+      [
+        "<script type='text/jsx'><%= rendered_example %></script> ",
+        "<div class=\"codeBlock jsExample\">",
+          "<div class=\"highlight\">",
+            "<pre>",
+              "<%= code_example %>",
+            "</pre>",
+          "</div>",
+        "</div>",
+      ].join('')
+    end
+
+    def unknown_example_template
+      [
+        "<div class=\"codeBlock\">",
+          "<div class=\"highlight\">",
+            "<pre>",
+              "<%= code_example %>",
+            "</pre>",
+          "</div>",
+        "</div>",
+      ].join('')
+    end
 
     def is_haml?
       markdown_language && markdown_language.include?('haml_example')
@@ -46,6 +130,10 @@ module Hologram
 
     def is_html?
       markdown_language && markdown_language.include?('html_example')
+    end
+
+    def is_slim?
+      markdown_language && markdown_language.include?('slim_example')
     end
 
     def is_js?
@@ -56,94 +144,33 @@ module Hologram
       markdown_language && markdown_language == 'jsx_example'
     end
 
-    def is_slim?
-      markdown_language && markdown_language == 'slim_example'
-    end
-
     def is_table?
       markdown_language && markdown_language.include?('example_table')
     end
+  end
 
-    def example_output(code_snippet)
-      [
-        "<div class=\"exampleOutput\">",
-          rendered_code_snippet(code_snippet),
-        "</div>",
-      ].join('')
+
+  class Example < Struct.new(:code)
+    def rendered_example
+      code
     end
 
-    def rendered_code_snippet(code_snippet)
-      if is_haml?
-        haml_engine(code_snippet).render(Object.new, {})
-      elsif is_slim?
-        slim_engine(code_snippet).render(Object.new, {})
-      else
-        code_snippet
-      end
+    def code_example
+      formatter.format(lexer.lex(code))
     end
 
-    def code_block(code_snippet, opts={})
-      extra_classes = opts[:extra_classes] || []
-      classes = extra_classes.insert(0, 'codeBlock')
-      [
-        "<div class=\"#{classes.join(' ')}\">",
-          "<div class=\"highlight\">",
-            "<pre>",
-              "#{formatter.format(lexer.lex(code_snippet))}",
-            "</pre>",
-          "</div>",
-        "</div>",
-      ].join('')
+    def get_binding
+      binding
     end
 
-    def code_example_rows
-      rows = code.split("\n\n")
-      rows.inject("") do |res, row|
-        res + code_example_row(row)
-      end
-    end
-
-    def code_example_row(code_snippet)
-      [
-        "<tr>",
-          "<th>",
-            example_output(code_snippet),
-          "</th>",
-          "<td>",
-            code_block(code_snippet),
-          "</td>",
-        "</tr>",
-      ].join('')
-    end
-
-    def haml_engine(code_snippet)
-      safe_require 'haml', markdown_language
-      Haml::Engine.new(code_snippet.strip)
-    end
-
-    def slim_engine(code_snippet)
-      safe_require 'slim', markdown_language
-      Slim::Template.new { code_snippet.strip }
-    end
-
-    def lexer
-      @_lexer ||= if is_html?
-        Rouge::Lexer.find('html')
-      elsif is_haml?
-        Rouge::Lexer.find('haml')
-      elsif is_slim?
-        Rouge::Lexer.find('slim')
-      elsif is_js?
-        Rouge::Lexer.find('js')
-      elsif is_jsx?
-        Rouge::Lexer.find('html')
-      else
-        Rouge::Lexer.find_fancy('guess', code)
-      end
-    end
+    private
 
     def formatter
       @_formatter ||= Rouge::Formatters::HTML.new(wrap: false)
+    end
+
+    def lexer
+      @_lexer ||= Rouge::Lexer.find_fancy('guess', code)
     end
 
     def safe_require(templating_library, language)
@@ -152,6 +179,69 @@ module Hologram
       rescue LoadError
         raise "#{templating_library} must be present for you to use #{language}"
       end
+    end
+  end
+
+  class HtmlExample < Example
+    private
+
+    def lexer
+      @_lexer ||= Rouge::Lexer.find('html')
+    end
+  end
+
+  class HamlExample < Example
+    def rendered_example
+      haml_engine.render(Object.new, {})
+    end
+
+    private
+
+    def haml_engine
+      safe_require 'haml', 'haml'
+      Haml::Engine.new(code.strip)
+    end
+
+    def slim_engine(code_snippet)
+      safe_require 'slim', markdown_language
+      Slim::Template.new { code_snippet.strip }
+    end
+
+    def lexer
+      @_lexer ||= Rouge::Lexer.find('haml')
+    end
+  end
+
+  class SlimExample < Example
+    def rendered_example
+      slim_engine.render(Object.new, {})
+    end
+
+    private
+
+    def slim_engine
+      safe_require 'slim', 'slim'
+      Slim::Template.new { code.strip }
+    end
+
+    def lexer
+      @_lexer ||= Rouge::Lexer.find('slim')
+    end
+  end
+
+  class JsExample < Example
+    private
+
+    def lexer
+      @_lexer ||= Rouge::Lexer.find('js')
+    end
+  end
+
+  class JsxExample < Example
+    private
+
+    def lexer
+      @_lexer ||= Rouge::Lexer.find('html')
     end
   end
 end
