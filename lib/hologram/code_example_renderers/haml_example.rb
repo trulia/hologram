9ba6fe0e493f67ad3c6834bcf788fcc860f86916
Geoff Pleiss <gpleiss@gmail.com>
2014-12-26 11:41:26 -0800

    Remove unnecessary slim_engine method from HamlExample

--- lib/hologram/code_example_renderers/haml_example.rb
+++ lib/hologram/code_example_renderers/haml_example.rb
@@ -12,11 +12,6 @@ module Hologram
         Haml::Engine.new(code.strip)
       end
 
-      def slim_engine(code_snippet)
-        safe_require 'slim', markdown_language
-        Slim::Template.new { code_snippet.strip }
-      end
-
       def lexer
         @_lexer ||= Rouge::Lexer.find('haml')
       end
